9dd0305f3616ee01b181836cc5291dfb5b89eb45
yves.hsu <jjohny51207@gmail.com>
2025-05-06 17:09:39 +0800

    fix: Modification of the transfer address

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -3,14 +3,15 @@ import { NextRequest, NextResponse } from "next/server";
 export const runtime = "nodejs";
 
 export async function POST(req: NextRequest) {
-try {
-const origin = req.nextUrl.origin;
-const res = NextResponse.redirect(new URL("/login", origin));
-// 刪除 Session Cookie
-res.cookies.delete({ name: "ncusession", path: "/" });
-return res;
-} catch (e) {
-console.error("Logout Error:", e);
-return new NextResponse("Internal Server Error", { status: 500 });
-}
+    try {
+        const origin = req.nextUrl.origin;
+        const appUrl = process.env.NEXT_PUBLIC_NCU_DEFAULT_PAGE!;
+        const res = NextResponse.redirect(`${appUrl}/login`);
+        // 刪除 Session Cookie
+        res.cookies.delete({ name: "ncusession", path: "/" });
+        return res;
+    } catch (e) {
+        console.error("Logout Error:", e);
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
 }
