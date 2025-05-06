7d003e100a27c8bdc1dce92d49f6b3e0f5d60fa7
yves.hsu <jjohny51207@gmail.com>
2025-05-06 20:07:18 +0800

    chore: Add console.log

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server";
 export const runtime = "nodejs";
 
 export async function POST() {
+    console.log("Enter logout POST");
     const APP_URL = process.env.NEXT_PUBLIC_NCU_DEFAULT_PAGE!;
     // 1) 刪除 own‐cookie，2) 回傳 307 並導向相對路徑 /login
     const res = NextResponse.redirect(`${APP_URL}/login`, 307);
