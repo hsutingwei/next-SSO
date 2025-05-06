35e750717fa40603d67eaa880ee4f851b9c08cd9
yves.hsu <jjohny51207@gmail.com>
2025-05-06 17:04:04 +0800

    chore: Delete code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
 // src/app/layout.tsx
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import MenuBar from "@/components/MenuBar";
 
